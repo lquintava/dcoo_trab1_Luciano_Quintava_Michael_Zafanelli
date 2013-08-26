51eb65d6c08682f0b0cc740bea5376ade1950580
Maicom <maicomdassi@gmail.com>
2013-08-26 20:06:14 -0300

    teste classe tenis

--- src/edu/faqi/dcoo/gc/ex04/Tenis.java
+++ src/edu/faqi/dcoo/gc/ex04/Tenis.java
@@ -9,14 +9,15 @@ public class Tenis {
 	private int pontuacao1;
 	private int pontuacao2;
 	
+	private boolean dupla;
+	
 	
 	public Tenis(Pessoa jogador1, Pessoa jogador2) {
 	this.jogador1 = jogador1;
 	this.jogador2 = jogador2;
-	
+	this.dupla = false;
 	this.pontuacao1 = 0;
-	this.pontuacao2 = 0;
-		
+	this.pontuacao2 = 0;		
 		
 	}
 	
@@ -25,8 +26,28 @@ public class Tenis {
 		this(jogador1, jogador2);
 		this.jogador1 = jogador3;
 		this.jogador2 = jogador4;
-					
+		this.dupla = true;					
 			
 		}
+	
+	public boolean isDuple(){
+		return this.dupla;		
+	}
+
+	public int getPontuacao1() {
+		return pontuacao1;
+	}
+
+	public void setPontuacao1(int pontuacao1) {
+		this.pontuacao1 = pontuacao1;
+	}
+
+	public int getPontuacao2() {
+		return pontuacao2;
+	}
+
+	public void setPontuacao2(int pontuacao2) {
+		this.pontuacao2 = pontuacao2;
+	}
 
 }
--- tst/edu/faqi/dcoo/gc/ex04/TenisTest.java
+++ tst/edu/faqi/dcoo/gc/ex04/TenisTest.java
@@ -7,8 +7,48 @@ import org.junit.Test;
 public class TenisTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInstaciacaoManoAMano() {
+
+		//1 - Atribuir valores
+		
+		Pessoa jog1 = new Pessoa();
+		Pessoa jog2 = new Pessoa();
+		
+		//2 - Acionar metodos
+		
+		Tenis tenis = new Tenis(jog1, jog2);
+		boolean dupla = tenis.isDuple();
+		int pontosJoga1 = tenis.getPontuacao1();
+		int pontosJoga2 = tenis.getPontuacao2();
+		
+		//3 - Avaliar resultados
+		
+		assertTrue("Deveria ser mano a mano", dupla == false);
+		assertTrue("Jogador 1 deveria começar com Zero", pontosJoga1 == 0);
+		assertTrue("Jogador 2 deveria começar com Zero", pontosJoga2 == 0);
+	}
+	
+	public void testInstaciacaoDupla() {
+
+		//1 - Atribuir valores
+		
+		Pessoa jog1 = new Pessoa();
+		Pessoa jog2 = new Pessoa();
+		Pessoa jog3 = new Pessoa();
+		Pessoa jog4 = new Pessoa();
+		
+		//2 - Acionar metodos
+		
+		Tenis tenis = new Tenis(jog1, jog2, jog3, jog4);
+		boolean dupla = tenis.isDuple();
+		int pontosJoga1 = tenis.getPontuacao1();
+		int pontosJoga2 = tenis.getPontuacao2();
+		
+		//3 - Avaliar resultados
+		
+		assertTrue("Deveria ser Dupla", dupla == true);
+		assertTrue("Jogador 1 deveria começar com Zero", pontosJoga1 == 0);
+		assertTrue("Jogador 2 deveria começar com Zero", pontosJoga2 == 0);
 	}
 
 }
