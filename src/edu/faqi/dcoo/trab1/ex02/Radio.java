481946bc9aa79d7d5d2ca2fb381adf75976e3d3b
lquintava <lquintava@gmail.com>
2013-09-16 21:47:14 -0300

    teste

--- src/edu/faqi/dcoo/trab1/ex02/Radio.java
+++ src/edu/faqi/dcoo/trab1/ex02/Radio.java
@@ -35,6 +35,4 @@ public class Radio extends MeioComunicacao {
 	public String imprime() {
 		return "Nome - " + this.nome  +"Estação - "+this.estacao + "Banda - " + this.banda;
 	}
-	
-	//oookmmc
 }
